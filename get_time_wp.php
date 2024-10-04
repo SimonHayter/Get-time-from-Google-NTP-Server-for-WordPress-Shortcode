608645b2fc8faadd1947ac23bd952e5c8ba7499f
Simon Hayter <contactme@simonhayter.co.uk>
2024-10-04 19:01:48 +0100

    Update get_time_wp.php
    Improved security.

--- get_time_wp.php
+++ get_time_wp.php
@@ -1,35 +1,29 @@
 function get_time_shortcode() {
-  // Google's public NTP servers
-  $ntpServers = [
-    'time1.google.com',
-    'time2.google.com',
-    'time3.google.com',
-    'time4.google.com'
-  ];
+	$ntpServers = ['time1.google.com', 'time2.google.com', 'time3.google.com', 'time4.google.com'];
 
-  // Try each server until we get a successful response
-  foreach ($ntpServers as $ntpServer) {
-    $socket = @fsockopen($ntpServer, 123, $errNo, $errStr, 1);
-    if ($socket) {
-      fputs($socket, "\x1b" . str_repeat("\0", 47));
-      $response = fread($socket, 48);
-      fclose($socket);
+	foreach ($ntpServers as $ntpServer) {
+		
+		// Use error handling instead of suppression
+		if ($socket = fsockopen($ntpServer, 123, $errNo, $errStr, 1)) { 
+			fputs($socket, "\x1b" . str_repeat("\0", 47));
+			$response = fread($socket, 48);
+			fclose($socket);
 
-      // Extract the timestamp from the response
-      $timestamp = unpack('N12', $response);
-      $timestamp = $timestamp[9] - 2208988800; // Convert NTP timestamp to Unix timestamp
+			// Validate the response data before using it
+			if (strlen($response) === 48) { 
+				date_default_timezone_set('Europe/London');
+				$currentTime = date('d/m/Y H:i:s', unpack('N12', $response)[9] - 2208988800);
+				return "<p>The current time in the UK is: " . $currentTime . "</p>";
 
-      // Set the timezone to London
-      date_default_timezone_set('Europe/London');
-
-      // Format the time in UK standard
-      $currentTime = date('d/m/Y H:i:s', $timestamp);
-      return "<p>The current time in the UK is: " . $currentTime . "</p>";
-    }
-  }
-
-  // If no server responds, return an error message
-  return "<p>Error: Could not retrieve time from Google NTP server.</p>";
+			// Else will trigger a log event	
+			} else {
+				// Log the error or handle it appropriately
+				error_log("Invalid response from NTP server: $ntpServer"); 
+			}
+		} else {
+		// Log the connection error
+			error_log("Error connecting to NTP server: $ntpServer - $errStr ($errNo)"); 
+		}
+	}
+	return "<p>Error: Could not retrieve time.</p>"; 
 }
-
-add_shortcode('get_time', 'get_time_shortcode');
